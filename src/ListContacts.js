f9b37014cd421443cf5a5fd5c811934b30195a67
test.hortela1n <pablohortelano2@gmail.com>
2020-04-13 19:24:32 +0200

    changing component for a stateless functional component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,11 +1,9 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-class ListContacts extends Component {
-    render(){
-        console.log('Props', this.props)
-        return(
-            <ol className = 'contact-list'>
-                {this.props.contacts.map(contact => (
+function ListContacts (props) {
+    return (
+        <ol className = 'contact-list'>
+                {props.contacts.map(contact => (
                    <li key = {contact.id} className = 'contact-list-item'> 
                         <div    
                         className = 'contact-avatar'
@@ -23,8 +21,7 @@ class ListContacts extends Component {
                    </li>
                 ))}
             </ol>
-        )
-    }
-}
+    )
+}    
 
 export default ListContacts
